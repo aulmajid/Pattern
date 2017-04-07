0c5fcc8a480511a72a4b1423ffd96592e1177791
Majid <aulmajid@gmail.com>
2017-04-07 20:03:45 +0700

    delete

--- src/pubsub/publishers/Facebook.java
+++
@@ -1,9 +0,0 @@
-package publishers;
-
-
-/**
- * Created by Majid on 07-Apr-17.
- */
-public class Facebook extends MultiplatformApps {
-
-}
--- src/pubsub/subscribers/Phone.java
+++
@@ -1,7 +0,0 @@
-package subscribers;
-
-/**
- * Created by Majid on 07-Apr-17.
- */
-public class Phone {
-}
